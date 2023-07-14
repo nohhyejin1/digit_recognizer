ffad07d29641e0c7f74c2b6f998caec53b5fe666
nohhyejin <shgpwls374@gmail.com>
2023-07-14 15:58:46 +0900

    HOOK TEST

--- train/train_test.py
+++ train/train_test.py
@@ -4,7 +4,7 @@ from train_new import train
 class Test(unittest.TestCase):
     def test_train(self):
         result = train()
-        self.assertGreater(result, 90)
+        self.assertGreater(result, 85)
 
 if __name__ == "__main__":
     unittest.main()
